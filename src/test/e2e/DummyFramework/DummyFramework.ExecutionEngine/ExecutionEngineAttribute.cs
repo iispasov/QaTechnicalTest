25e4a6252a75acb44797ba37244c06a5b40b76b8
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-06 23:29:24 +0100

    Added class/method level attribute to control the execution engine and the type of browser

---
+++ src/test/e2e/DummyFramework/DummyFramework.ExecutionEngine/ExecutionEngineAttribute.cs
@@ -0,0 +1,24 @@
+﻿using DummyFramework.Core.Enums;
+using DummyFramework.ExecutionEngine.Enums;
+using System;
+
+namespace DummyFramework.ExecutionEngine
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ExecutionEngineAttribute : Attribute
+    {
+        public ExecutionEngineAttribute(ExecutionEngineType executionEngineType, Browsers browser)
+        {
+            Browser = browser;
+            ExecutionEngineType = executionEngineType;
+        }
+
+        public ExecutionEngineAttribute(Browsers browser) : this(ExecutionEngineType.WebDriver, browser)
+        {
+        }
+
+        public Browsers Browser { get; set; }
+
+        public ExecutionEngineType ExecutionEngineType { get; set; }
+    }
+}
