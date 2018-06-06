8aeec4b81a71a0e38cc35de887e8f9ac7a473c32
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-06 21:48:56 +0100

    Added extension method to retrieve global Unity container

---
+++ src/test/e2e/DummyFramework/DummyFramework.Utilities/UnityContainerFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Unity;
+
+namespace DummyFramework.Utilities
+{
+    public static class UnityContainerFactory
+    {
+        private static IUnityContainer _unityContainer;
+
+        static UnityContainerFactory()
+        {
+            _unityContainer = new UnityContainer();
+        }
+
+        public static IUnityContainer GetCurrentContainer()
+        {
+            return _unityContainer;
+        }
+
+        public static bool IsContainerEmpty()
+        {
+            return !_unityContainer.Registrations.Any();
+        }
+
+        public static void ResetContainer()
+        {
+            _unityContainer = new UnityContainer();
+        }
+    }
+}
