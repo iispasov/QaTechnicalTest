d88d853e0b4c6a7af342e72a25855cbef42eb876
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-06 23:08:11 +0100

    Implemented base test class which can be easily extended via custom behaviours

--- src/test/e2e/DummyFramework/DummyFramework.TestBase/TestBase.cs
+++ src/test/e2e/DummyFramework/DummyFramework.TestBase/TestBase.cs
@@ -1,6 +1,104 @@
-﻿namespace DummyFramework.TestBase
+﻿using DummyFramework.Core.Driver;
+using DummyFramework.Utilities;
+using NUnit.Framework;
+using System.Reflection;
+using Unity;
+
+namespace DummyFramework.TestBase
 {
+    [TestFixture]
     public class TestBase
     {
+        private readonly TestExecutionProvider _currentTestExecutionProvider;
+        private IDriver _driver;
+
+        public TestBase()
+        {
+            if (!UnityContainerFactory.IsContainerEmpty())
+            {
+                UnityContainerFactory.ResetContainer();
+            }
+
+            Container = UnityContainerFactory.GetCurrentContainer();
+            Container.RegisterInstance(Container);
+            _currentTestExecutionProvider = new TestExecutionProvider();
+        }
+
+        protected IDriver Driver => _driver;
+        protected IUnityContainer Container { get; }
+        private static string TestName => TestContext.CurrentContext.Test.Name;
+
+        [SetUp]
+        protected void CoreTestInit()
+        {
+            var testInfo = GetCurrentTestInfo();
+
+            _currentTestExecutionProvider.PreTestInit(TestName, testInfo);
+            TestInit();
+            _currentTestExecutionProvider.PostTestInit(TestName, testInfo);
+        }
+
+        [TearDown]
+        protected void CoreTestCleanup()
+        {
+            var testInfo = GetCurrentTestInfo();
+
+            _currentTestExecutionProvider.PreTestCleanup(TestName, testInfo);
+            TestCleanup();
+            _currentTestExecutionProvider.PostTestCleanup(TestName, testInfo);
+        }
+
+        [OneTimeSetUp]
+        protected void CoreTestFixtureInit()
+        {
+            var fixtureClassInfo = GetType();
+
+            InitializeTestExecutionBehaviours(_currentTestExecutionProvider);
+
+            _currentTestExecutionProvider.PreTestFixtureInit(fixtureClassInfo);
+            TestFixtureInit();
+            _currentTestExecutionProvider.PostTestFixtureInit(fixtureClassInfo);
+        }
+
+        [OneTimeTearDown]
+        protected void CoreTestFixtureCleanup()
+        {
+            var fixtureClassInfo = GetType();
+
+            _currentTestExecutionProvider.PreTestFixtureCleanup(fixtureClassInfo);
+            TestFixtureCleanup();
+            _currentTestExecutionProvider.PostTestFixtureCleanup(fixtureClassInfo);
+        }
+
+        protected virtual void TestInit()
+        {
+        }
+
+        protected virtual void TestCleanup()
+        {
+        }
+
+        protected virtual void TestFixtureInit()
+        {
+        }
+
+        protected virtual void TestFixtureCleanup()
+        {
+        }
+
+        private static void InitializeTestExecutionBehaviours(IExecutionProvider testExecutionProvider)
+        {
+            var observers = UnityContainerFactory.GetCurrentContainer().ResolveAll<ITestBehaviourObserver>();
+
+            foreach (var observer in observers)
+            {
+                observer.Subscribe(testExecutionProvider);
+            }
+        }
+
+        private MethodInfo GetCurrentTestInfo()
+        {
+            return GetType().GetMethod(TestContext.CurrentContext.Test.MethodName);
+        }
     }
 }
