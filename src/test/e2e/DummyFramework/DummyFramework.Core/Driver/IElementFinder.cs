85d19cd8b8638c18a017eba8c33eea626ce96024
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-06 21:17:48 +0100

    Added basic generic methods for finding elements

--- src/test/e2e/DummyFramework/DummyFramework.Core/Driver/IElementFinder.cs
+++ src/test/e2e/DummyFramework/DummyFramework.Core/Driver/IElementFinder.cs
@@ -1,6 +1,14 @@
-﻿namespace DummyFramework.Core.Driver
+﻿using DummyFramework.Core.Controls;
+using System.Collections.Generic;
+
+namespace DummyFramework.Core.Driver
 {
     public interface IElementFinder
     {
+        TElement Find<TElement>(By by) where TElement : class, IElement;
+
+        IEnumerable<TElement> FindAll<TElement>(By by) where TElement : class, IElement;
+
+        bool IsElementPresent(By by);
     }
 }
