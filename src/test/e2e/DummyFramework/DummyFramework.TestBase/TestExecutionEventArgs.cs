3307d437b14e9ec3ee328d57314a80d4aed672c2
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-06 21:55:55 +0100

    Added TestExecutionEventArgs arguments to support the test execution workflow events

---
+++ src/test/e2e/DummyFramework/DummyFramework.TestBase/TestExecutionEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace DummyFramework.TestBase
+{
+    public class TestExecutionEventArgs : EventArgs
+    {
+        public TestExecutionEventArgs(string testName, MemberInfo memberInfo)
+        {
+            TestName = testName;
+            MemberInfo = memberInfo;
+        }
+
+        public virtual string TestName { get; }
+
+        public virtual MemberInfo MemberInfo { get; }
+    }
+}
