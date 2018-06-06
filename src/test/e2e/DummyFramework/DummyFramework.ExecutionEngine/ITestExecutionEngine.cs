dfbc2fb447efdbb080cbdae23c6192a08892018d
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-06 23:37:59 +0100

    Added base class for each supported test execution engine/tool (e.g. Selenium)

---
+++ src/test/e2e/DummyFramework/DummyFramework.ExecutionEngine/ITestExecutionEngine.cs
@@ -0,0 +1,12 @@
+﻿using DummyFramework.Core.Enums;
+using System.Reflection;
+
+namespace DummyFramework.ExecutionEngine
+{
+    public interface ITestExecutionEngine
+    {
+        bool IsSelected(MemberInfo memberInfo);
+
+        void RegisterDependencies(Browsers browserSettings);
+    }
+}
---
+++ src/test/e2e/DummyFramework/DummyFramework.ExecutionEngine/TestExecutionEngineBase.cs
@@ -0,0 +1,36 @@
+﻿using DummyFramework.Core.Driver;
+using DummyFramework.Core.Enums;
+using System.Reflection;
+using Unity;
+
+namespace DummyFramework.ExecutionEngine
+{
+    public abstract class TestExecutionEngineBase<TExecutionEngineAttribute> : ITestExecutionEngine
+        where TExecutionEngineAttribute : ExecutionEngineAttribute
+    {
+        protected readonly IUnityContainer Container;
+        protected IDriver Driver;
+
+        protected TestExecutionEngineBase(IUnityContainer container)
+        {
+            Container = container;
+        }
+
+        public bool IsSelected(MemberInfo memberInfo)
+        {
+            var isSetOnMethodLevel = TryGetExecutionEngineAttribute(memberInfo);
+            var isSetOnClassLevel = TryGetExecutionEngineAttribute(memberInfo.DeclaringType);
+
+            return isSetOnMethodLevel || isSetOnClassLevel;
+        }
+
+        public abstract void RegisterDependencies(Browsers browserSettings);
+
+        private static bool TryGetExecutionEngineAttribute(MemberInfo currentType)
+        {
+            var classExecutionEngineAttribute = currentType?.GetCustomAttribute<TExecutionEngineAttribute>(true);
+
+            return classExecutionEngineAttribute != null;
+        }
+    }
+}
