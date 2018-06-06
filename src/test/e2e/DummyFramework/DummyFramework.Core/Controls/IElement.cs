15e5a297b56abc0db823f0cadcc7bd852b19d1cc
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-06 21:21:04 +0100

    Added common properties/methods for IElement

--- src/test/e2e/DummyFramework/DummyFramework.Core/Controls/IElement.cs
+++ src/test/e2e/DummyFramework/DummyFramework.Core/Controls/IElement.cs
@@ -4,6 +4,24 @@ namespace DummyFramework.Core.Controls
 {
     public interface IElement : IElementFinder
     {
+        bool IsVisible { get; }
 
+        bool IsEnabled { get; }
+
+        int Width { get; }
+
+        string CssClass { get; }
+
+        string Content { get; }
+
+        string GetAttribute(string name);
+
+        void WaitForExists();
+
+        void WaitForNotExists();
+
+        void Click();
+
+        void MouseClick();
     }
 }
