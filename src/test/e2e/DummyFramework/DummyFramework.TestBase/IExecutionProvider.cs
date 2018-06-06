5b97f39d5ee7e1669a0bd02a198a0bf71400719b
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-06 22:02:29 +0100

    Execution provider interface allows us to 'decorate' the normal tests execution flow with custom events

---
+++ src/test/e2e/DummyFramework/DummyFramework.TestBase/IExecutionProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DummyFramework.TestBase
+{
+    public interface IExecutionProvider
+    {
+        event EventHandler<TestExecutionEventArgs> PreTestFixtureInitEvent;
+
+        event EventHandler<TestExecutionEventArgs> PostTestFixtureInitEvent;
+
+        event EventHandler<TestExecutionEventArgs> PreTestFixtureCleanupEvent;
+
+        event EventHandler<TestExecutionEventArgs> PostTestFixtureCleanupEvent;
+
+        event EventHandler<TestExecutionEventArgs> PreTestInitEvent;
+
+        event EventHandler<TestExecutionEventArgs> PostTestInitEvent;
+
+        event EventHandler<TestExecutionEventArgs> PreTestCleanupEvent;
+
+        event EventHandler<TestExecutionEventArgs> PostTestCleanupEvent;
+    }
+}
