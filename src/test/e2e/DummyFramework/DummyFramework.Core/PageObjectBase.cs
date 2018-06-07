19606df45e5acf86c6ebfd6182f10676fbb59b16
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-07 04:49:46 +0100

    Added page objects base class

---
+++ src/test/e2e/DummyFramework/DummyFramework.Core/PageObjectBase.cs
@@ -0,0 +1,14 @@
+﻿using DummyFramework.Core.Driver;
+
+namespace DummyFramework.Core
+{
+    public abstract class PageObjectBase
+    {
+        protected PageObjectBase(IElementFinderService elementFinder)
+        {
+            ElementFinder = elementFinder;
+        }
+
+        protected IElementFinderService ElementFinder { get; private set; }
+    }
+}
