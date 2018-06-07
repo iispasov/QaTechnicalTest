49439e857c763f2c8d506d2348b8e1d370e58d32
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-07 19:27:13 +0100

    Implemented InputSubmit for Selenium

---
+++ src/test/e2e/DummyFramework/DummyFramework.Selenium/Controls/InputSubmit.cs
@@ -0,0 +1,13 @@
+﻿using DummyFramework.Core.Controls;
+using OpenQA.Selenium;
+using Unity;
+
+namespace DummyFramework.Selenium.Controls
+{
+    public class InputSubmit : ContentElement, IInputSubmit
+    {
+        public InputSubmit(IWebDriver driver, IWebElement element, IUnityContainer container) : base(driver, element, container)
+        {
+        }
+    }
+}
