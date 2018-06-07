cd8811dcf7b8d37edb6dffd81a9d04d23844b1dd
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-07 04:50:45 +0100

    Added base class for simple web pages

---
+++ src/test/e2e/DummyFramework/DummyFramework.Core/WebPageBase.cs
@@ -0,0 +1,14 @@
+﻿using DummyFramework.Core.Driver;
+
+namespace DummyFramework.Core
+{
+    public abstract class WebPageBase : PageObjectBase
+    {
+        protected WebPageBase(IElementFinderService elementFinder, INavigationService navigationService) : base(elementFinder)
+        {
+            NavigationService = navigationService;
+        }
+
+        protected INavigationService NavigationService { get; private set; }
+    }
+}
