c86209a29abf3d81cb21beface89db59460abdd3
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-07 06:58:30 +0100

    Added concrete implementation of TestExecutionEngine for Selenium

---
+++ src/test/e2e/DummyFramework/DummyFramework.Selenium/TestExecutionEngine.cs
@@ -0,0 +1,43 @@
+﻿using DummyFramework.Core;
+using DummyFramework.Core.Controls;
+using DummyFramework.Core.Driver;
+using DummyFramework.Core.Enums;
+using DummyFramework.ExecutionEngine;
+using DummyFramework.Selenium.Controls;
+using DummyFramework.Selenium.TestEngine;
+using Unity;
+
+namespace DummyFramework.Selenium
+{
+    public class TestExecutionEngine : TestExecutionEngineBase<ExecutionEngineAttribute>
+    {
+        public TestExecutionEngine(IUnityContainer container) : base(container)
+        {
+        }
+
+        public override void RegisterDependencies(Browsers browserType)
+        {
+            var browserSettings = new BrowserSettings(browserType);
+
+            Driver = new SeleniumDriver(browserSettings);
+            RegisterControls();
+            RegisterDriver();
+        }
+
+        private void RegisterControls()
+        {
+            Container.RegisterType<IElement, Element>();
+            Container.RegisterType<IContentElement, ContentElement>();
+            Container.RegisterType<IButton, Button>();
+        }
+
+        private void RegisterDriver()
+        {
+            Container.RegisterInstance(Driver);
+            Container.RegisterInstance<IElementFinderService>(Driver);
+            Container.RegisterInstance<INavigationService>(Driver);
+            Container.RegisterInstance<IJavaScriptExecutor>(Driver);
+            Container.RegisterInstance<IBrowserService>(Driver);
+        }
+    }
+}
