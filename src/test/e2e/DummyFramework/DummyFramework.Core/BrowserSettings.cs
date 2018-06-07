9f83901514e2f0bc19f54d0bc99aaadf9bf9e7a2
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-07 05:05:03 +0100

    Added BrowserSettings to control the browser type, default wait timeouts etc.

---
+++ src/test/e2e/DummyFramework/DummyFramework.Core/BrowserSettings.cs
@@ -0,0 +1,47 @@
+﻿using DummyFramework.Core.Enums;
+using System;
+
+namespace DummyFramework.Core
+{
+    public sealed class BrowserSettings
+    {
+        public BrowserSettings(Browsers type)
+        {
+            Type = type;
+        }
+
+        public static BrowserSettings DefaultChromeSettings => new BrowserSettings(Browsers.Chrome)
+        {
+            BrowserExeDirectory = string.Empty,
+            PageLoadTimeout = 60,
+            ScriptTimeout = 60,
+            ElementsWaitTimeout = 60
+        };
+
+        public static BrowserSettings DefaultFirefoxSettings => new BrowserSettings(Browsers.Firefox)
+        {
+            BrowserExeDirectory = Environment.CurrentDirectory,
+            PageLoadTimeout = 60,
+            ScriptTimeout = 60,
+            ElementsWaitTimeout = 60
+        };
+
+        public static BrowserSettings DefaultInternetExplorerSettings => new BrowserSettings(Browsers.InternetExplorer)
+        {
+            BrowserExeDirectory = Environment.CurrentDirectory,
+            PageLoadTimeout = 60,
+            ScriptTimeout = 60,
+            ElementsWaitTimeout = 60
+        };
+
+        public Browsers Type { get; private set; }
+
+        public int ScriptTimeout { get; set; }
+
+        public int PageLoadTimeout { get; set; }
+
+        public int ElementsWaitTimeout { get; set; }
+
+        public string BrowserExeDirectory { get; set; }
+    }
+}
