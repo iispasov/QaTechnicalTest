3ac20a1619d524e7904a16ef3daf0223f850c27f
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-07 06:46:54 +0100

    Added attribute to control the type of browser

---
+++ src/test/e2e/DummyFramework/DummyFramework.Selenium/WebDriverExecutionEngineAttribute.cs
@@ -0,0 +1,13 @@
+﻿using DummyFramework.Core.Enums;
+using DummyFramework.ExecutionEngine;
+
+namespace DummyFramework.Selenium
+{
+    public class WebDriverExecutionEngineAttribute : ExecutionEngineAttribute
+    {
+        public WebDriverExecutionEngineAttribute(Browsers browser = Browsers.Chrome) : base(browser)
+        {
+            Browser = browser;
+        }
+    }
+}
