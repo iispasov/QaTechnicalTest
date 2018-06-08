0a714120c013eaef0cee0e1941e3c6a704ea477a
Iskren Spasov <i.i.spasov@gmail.com>
2018-06-08 07:58:30 +0100

    Challanges implementation

---
+++ src/test/e2e/AutomatedTests/AutomatedTests.Challenges/ArraysChallange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DummyFramework.Core.Controls;
+
+namespace AutomatedTests.Challenges
+{
+    public static class ArraysChallange
+    {
+        public static int? FindCenter(IEnumerable<IElement> rowCells)
+        {
+            var cellValues = rowCells.Select(x => Convert.ToInt32(x.Content)).ToArray();
+            var totalSum = cellValues.Sum();
+            var currSum = 0;
+            var result = default(int?);
+
+            for (var i = 0; i < cellValues.Length; i++)
+            {
+                var currVal = cellValues[i];
+                currSum += cellValues[i];
+
+                if (currSum - currVal == (totalSum - currVal) / 2)
+                {
+                    result = i;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/test/e2e/AutomatedTests/AutomatedTests.Challenges/Challenges.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using Unity;
+using AutomatedTests.PageObjects.ChallengeSection;
+using AutomatedTests.PageObjects.HomeSection;
+
+namespace AutomatedTests.Challenges
+{
+    public class AutomationChallenges
+    {
+        private IUnityContainer _container;
+
+        public AutomationChallenges(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public void StartChallengeOne()
+        {
+            var homeSection = _container.Resolve<HomeSection>();
+
+            homeSection.ClickButtonRenderChallenge();
+        }
+
+        public void StartChallengeTwo()
+        {
+            var challengeSection = _container.Resolve<ChallengeSection>();
+
+            challengeSection.WaitForDisplayed();
+        }
+
+        public void StartChallengeThree()
+        {
+            var tableArrays = _container.Resolve<TableArrays>();
+            var rowCount = tableArrays.GetTableRows().Count();
+            var submitAnswersForm = _container.Resolve<SubmitForm>();
+
+            for (var i = 1; i <= rowCount; i++)
+            {
+                var cells = tableArrays.GetRowCells(i);
+                var res = ArraysChallange.FindCenter(cells);
+
+                submitAnswersForm.EnterTextInputSubmit(i, res.ToString());
+            }
+        }
+
+        public void SubmitAnswers(string name)
+        {
+            var submitAnswersForm = _container.Resolve<SubmitForm>();
+
+            submitAnswersForm.EnterTextInputSubmit(4, "Iskren");
+            submitAnswersForm.ClickButtonSubmitAnswers();
+        }
+    }
+}
